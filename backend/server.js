dd8ad556e49b4fa6207d112082ad122a2f2f850a
Maryna Ozirska <m.ozirska2000@gmail.com>
2024-02-06 23:29:53 +0100

    vercel

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const corsOptions = {
-  origin: "https://recipes-4bzor32cd-marinas-projects-dc2f1bb9.vercel.app",
+  origin: "https://recipes-app-sable.vercel.app",
   credentials: true,
 };
 
